945c5303ccf5776460cd84931a8b8ca3e9ab3907
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-12-11 11:01:43 -0500

    Initial Commit

---
+++ ISBNIdentificationApp/ISBNIdentificationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISBNIdentificationAppApp.swift
+//  ISBNIdentificationApp
+//
+//  Created by Berk Bozkurt on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct ISBNIdentificationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
